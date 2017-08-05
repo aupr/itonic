85c074dcfd887d7c76f7b0702a2d035ec5340d14
Aman Ullah <amanullah.en@gmail.com>
2017-08-05 23:19:07 +0600

    itonic example query parser complete

--- script/main.js
+++ script/main.js
@@ -1,11 +1,23 @@
 $(function () {
-    $('#btn-modal').click(function () {
-        itonic.modal.execute({
-            createButton: "yes, No",
-            crossButtonEnable: true
-        });
-        //alert();
-        //console.log(it.modal().execute());
+    var $_get = it.getURIQuery();
+    if (typeof $_get.key3 === "string"){
+        $("#uri-query-view").text(JSON.stringify($_get));
+        $("#uri-query-code").text("it.getURIQuery()");
+    }
+    else {
+        $("#uri-query-view").text(JSON.stringify(it.getURIQuery(true)));
+        $("#uri-query-code").text("it.getURIQuery(true)");
+    }
+
+    $("#parse-uri-custom").click(function () {
+        if ($("#uri-query-consider-array").is(":checked")) {
+            $("#uri-query-view").text(JSON.stringify(it.getURIQuery(true, $("#custom-url").val())));
+            $("#uri-query-code").text("it.getURIQuery(true, "+$("#custom-url").val()+")");
+        } else {
+            $("#uri-query-view").text(JSON.stringify(it.getURIQuery(false, $("#custom-url").val())));
+            $("#uri-query-code").text("it.getURIQuery(false, "+$("#custom-url").val()+")");
+        }
+
     });
 });
 
